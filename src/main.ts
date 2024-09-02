bc7a0008d2cfcaf5b35d0a94af89d1cdbc367c56
mateopedehonta <mateopedehonta@gmail.com>
2024-09-02 10:27:06 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(

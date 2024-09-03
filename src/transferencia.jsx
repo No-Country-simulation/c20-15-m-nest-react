348837f9e561b101d83e7d7c930ed6a549146c57
NaNajli <nadurodriguez2@gmail.com>
2024-09-03 14:02:07 -0300

    Esqueleto pagina transferencia

---
+++ src/transferencia.jsx
@@ -0,0 +1,42 @@
+
+
+    {/* wireframe uno */}
+    <div> 
+        <h1>Bienvenido, nombre de usuario</h1>
+        <select>
+        <i>persona icono</i>
+        <p>Cuenta</p>
+        <p><strong>Disponible</strong></p>
+        </select>
+       
+    </div>
+
+     {/* wireframe 2 */}
+     <div>
+
+     </div>
+
+      {/* wireframe */}
+    <div>
+    <h1>Origen y destino</h1>
+    <input type="text" placeholder="Banco Santander"  />
+    <h2>Monto y transferencia </h2>
+    <input type="text" placeholder="$500"  />
+    <input type="text" placeholder="Comentario Opcional"  />
+    <select>
+    <option value="someOption">Autorizar con clave secreta</option>
+    <option value="otherOption">Otra opción</option>
+     </select>
+    <button>Continuar</button>
+    </div>
+    
+{/* FOOTER FIXED */}
+
+<select>
+        <option value="">Inicio</option>
+        <option value="">Transferir</option>
+        <option value="">Pagar</option>
+        <option value="">Ayuda</option>
+        </select>
+       
+       

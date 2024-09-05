663d4a037ee94bba951036e31c8382d67242b00b
mateopedehonta <mateopedehonta@gmail.com>
2024-09-05 13:51:12 -0300

    Login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Login } from "./componentes/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div>
+      <header>
+        <h1> Banca Digital</h1>
+      </header>
       <div>
-        <header>
-          <h1> Banca Digital</h1>
-        </header>
+        <Login />
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
+const useLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null); // Nuevo estado para almacenar el usuario
+
+  const Api = axios.create({
+    baseURL: `${import.meta.env.VITE_PUBLIC_DATABASE_URL}/`,
+  });
+
+  const login = async (email, password) => {
+    setLoading(true);
+    setError(null);
+    try {
+      // Realiza la petición HTTP para autenticar el usuario
+      const response = await Api.post(`/auth/login`, {
+        email,
+        password,
+      });
+
+      // Supongamos que la respuesta contiene un token JWT
+      const token = response.data.token;
+      // Almacenar el token en localStorage o cookies
+      localStorage.setItem("token", token);
+
+      // Actualizar el estado de autenticación
+      setIsAuthenticated(true);
+    } catch (err) {
+      // Manejar el error
+      setError(err.response?.data?.message || "Error en la autenticación");
+      setIsAuthenticated(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getUser = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) throw new Error("No token found");
+
+      // Enviar el token en el encabezado Authorization
+      const response = await Api.get(`/user`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      // Guardar los datos del usuario en el estado
+      setUser(response.data);
+      setIsAuthenticated(true);
+    } catch (err) {
+      setError(err.response?.data?.message || "Error al obtener el usuario");
+      setUser(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      getUser(); // Verifica el token y obtiene el usuario si es válido
+    }
+  }, []);
+  return { login, getUser, loading, error, isAuthenticated, user };
+};
+
+export default useLogin;

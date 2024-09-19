099af18a23ae051e9f6592051625a7234fd3fdf5
nespinoza2000 <nespinozasegovia@gmail.com>
2024-09-19 09:14:51 -0400

    cambios en el css

--- src/components/transfer/TransferPage.jsx
+++ src/components/transfer/TransferPage.jsx
@@ -3,9 +3,10 @@ import { ContactList } from "./ContactList";
 
 export const TransferPage = () => {
   return (
-    <div>
+    <div className="tcontact">
       <Link to={"/transfer/newcontact"}>transferir a nuevo contacto</Link>
       <ContactList />
+      <h1>¿A donde queres transferir?</h1>
     </div>
   );
 };

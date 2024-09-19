fa97968854697a668093043f9c7a5542ee25801f
nespinoza2000 <nespinozasegovia@gmail.com>
2024-09-19 11:24:02 -0400

    css modificado

--- src/components/transfer/TransferPage.jsx
+++ src/components/transfer/TransferPage.jsx
@@ -7,6 +7,15 @@ export const TransferPage = () => {
       <Link to={"/transfer/newcontact"}>transferir a nuevo contacto</Link>
       <ContactList />
       <h1>¿A donde queres transferir?</h1>
+      <div className="terceros"> {/* Opcion: A terceros */}
+        <h3>A terceros </h3>
+      </div>
+      <div className="ecuentas"> {/* Opcion: entre tus cuentas */}
+        <h3>Entre tus cuentas</h3>
+      </div>
+      <div className="ndestinatario"> {/* Opcion: A un nuevo destinatario */}
+        <h3>A un nuevo destinatario</h3>
+      </div>
     </div>
   );
 };

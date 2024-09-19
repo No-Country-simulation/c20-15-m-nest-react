f1ef3d708d590f23fa34087f63bce05a1737fc29
nespinoza2000 <nespinozasegovia@gmail.com>
2024-09-19 13:45:26 -0400

    modificacion del css

--- src/components/transfer/TransferPage.jsx
+++ src/components/transfer/TransferPage.jsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom";
 import { ContactList } from "./ContactList";
+import { FiHome } from "react-icons/fi";
+import { TbTransferIn } from "react-icons/tb";
+import { MdOutlinePayment } from "react-icons/md";
+import { IoHelpCircleOutline } from "react-icons/io5";
 
 export const TransferPage = () => {
   return (
@@ -16,6 +20,24 @@ export const TransferPage = () => {
       <div className="ndestinatario"> {/* Opcion: A un nuevo destinatario */}
         <h3>A un nuevo destinatario</h3>
       </div>
+      <section className="footerTP"> {/*classname modificado para la pagina de transferencia*/}
+        <Link className="none" to={"/transfer"}>
+          <p>Inicio</p>
+          <FiHome />
+        </Link>
+        <Link className="none" to={"/transfer"}>
+          <p>Transf</p>
+          <TbTransferIn />
+        </Link>
+        <Link className="none" to={"/transfer"}>
+          <p>Pagar</p>
+          <MdOutlinePayment />
+        </Link>
+        <Link className="none" to={"/transfer"}>
+          <p>Ayuda</p>
+          <IoHelpCircleOutline />
+        </Link>
+      </section>
     </div>
   );
 };
